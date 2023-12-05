f7d19d79a7c9efa3bc150f929850bbeb50e5a5d0
Artyom <singlespreekrsk@gmail.com>
2023-12-06 00:54:55 +0300

    git init

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button";
+
+const state = true;
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-indigo-500">Dis init</p>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+}
