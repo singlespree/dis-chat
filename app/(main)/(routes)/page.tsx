8b386b87fd39b7052d5c13039021215a39e8028f
Artyom <singlespreekrsk@gmail.com>
2023-12-06 11:03:25 +0300

    create auth + dark mode and init back prisma

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,14 +1,11 @@
-import {Button} from "@/components/ui/button";
-
-const state = true;
+import { UserButton } from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
-  return (
-    <div>
-      <p className="text-3xl font-bold text-indigo-500">Dis init</p>
-      <Button>
-        Click Me
-      </Button>
-    </div>
-  )
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/"/>
+            <ModeToggle />
+        </div>
+    )
 }
