26e5d762ea3c974bf74d1d277c976de2efa9632b
Artyom <singlespreekrsk@gmail.com>
2023-12-08 22:20:53 +0300

    server invites

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -14,6 +14,7 @@ import {
   Settings,
   Users, PlusCircle, Trash, LogOut
 } from "lucide-react";
+import {useModal} from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles
@@ -24,6 +25,8 @@ export const ServerHeader = ({
   server,
   role
 }: ServerHeaderProps) => {
+  const {onOpen} = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -45,6 +48,7 @@ export const ServerHeader = ({
       >
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", {server})}
             className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
           >
             Invite People
