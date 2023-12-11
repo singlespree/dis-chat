ce9a696af516e278df330574345d15b41fb45c3a
Artyom <singlespreekrsk@gmail.com>
2023-12-11 18:48:09 +0300

    Add channel edit and delete functionality
    Added new modals for deleting and editing channels in the server. Updated modal-store hooks with new types for these modals and implemented API routes for channel deletion and modification. Also applied related changes in server-channel component enabling these functionalities.

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -10,6 +10,7 @@ import {Edit, Hash, Lock, Mic, Trash, Video} from "lucide-react";
 import {useParams, useRouter} from "next/navigation";
 import {cn} from "@/lib/utils";
 import {ActionToolTip} from "@/components/action-tooltip";
+import {ModalType, useModal} from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -28,6 +29,7 @@ export const ServerChannel = ({
   server,
   role
 }: ServerChannelProps) => {
+  const {onOpen} = useModal()
   const params = useParams();
   const router = useRouter()
 
@@ -52,11 +54,13 @@ export const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionToolTip label="Edit">
             <Edit
+              onClick={() => onOpen("editChannel", {server, channel})}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionToolTip>
           <ActionToolTip label="Delete">
             <Trash
+              onClick={() => onOpen("deleteChannel", {server, channel})}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionToolTip>
