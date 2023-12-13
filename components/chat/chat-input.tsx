638dd87ddd39bc1740195a04439d8f0cdd03cc25
Artyom <singlespreekrsk@gmail.com>
2023-12-13 22:30:47 +0300

    Add emoji picker to chat input
    Introduced an emoji picker to the chat input form. The relevant dependencies (@emoji-mart/data and @emoji-mart/react) were added and a new EmojiPicker component was created. Changes also include creating a new popover component and making adjustments to chat input component to integrate the emoji picker. The chat is now more interactive and user-friendly with the addition of emojis.

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -13,8 +13,10 @@ import {
   FormItem,
 } from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
-import {Plus, Smile} from "lucide-react";
+import {Plus} from "lucide-react";
 import {useModal} from "@/hooks/use-modal-store";
+import {EmojiPicker} from "@/components/emoji-picker";
+import {useRouter} from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -33,7 +35,8 @@ export const ChatInput = ({
   name,
   type
 }: ChatInputProps) => {
-  const {onOpen} = useModal()
+  const {onOpen} = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -52,8 +55,11 @@ export const ChatInput = ({
       });
 
       await axios.post(url, values)
+
+      form.reset();
+      router.refresh();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
@@ -81,7 +87,9 @@ export const ChatInput = ({
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji:string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
                   </div>
                 </div>
               </FormControl>
