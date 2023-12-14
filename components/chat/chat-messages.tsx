a1c4e05dea6c74946b54ce8da4d0ce5fbfa04d3d
Artyom <singlespreekrsk@gmail.com>
2023-12-14 14:05:13 +0300

    Refactor chat message component and add controls
    This commit refactors the chat message component to use a new `ChatItem` component. A `DeleteMessageModal` is introduced for deleting messages, and the server now handles message deletion requests. It also adds the ability to edit existing messages. The `date-fns` package was introduced for handling date and time formatting.

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,10 @@ import {ChatWelcome} from "@/components/chat/chat-welcome";
 import {useChatQuery} from "@/hooks/use-chat-query";
 import {Loader2, ServerCrash} from "lucide-react";
 import {Fragment} from "react";
+import {format} from "date-fns"
+import {ChatItem} from "@/components/chat/chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 interface ChatMessagesProps {
   name: string;
@@ -83,9 +87,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>
-                {message.content}
-              </div>
+              <ChatItem
+                currentMember={member}
+                member={message.member}
+                key={message.id}
+                id={message.id}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
